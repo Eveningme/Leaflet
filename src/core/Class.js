f9dc3da127adaa3fabce67bb93edb1b1b2a1c7f8
Eveningme <eveningme@gmail.com>
2012-09-12 15:36:39 +0800

    翻译
    Class文件翻译

--- src/core/Class.js
+++ src/core/Class.js
@@ -7,6 +7,7 @@ L.Class = function () {};
 L.Class.extend = function (/*Object*/ props) /*-> Class*/ {
 
 	// extended class with the new prototype
+	// 用新的原型拓展类
 	var NewClass = function () {
 		if (this.initialize) {
 			this.initialize.apply(this, arguments);
@@ -14,6 +15,7 @@ L.Class.extend = function (/*Object*/ props) /*-> Class*/ {
 	};
 
 	// instantiate class without calling constructor
+	// 不调用构造函数实例化类
 	var F = function () {};
 	F.prototype = this.prototype;
 
@@ -22,26 +24,31 @@ L.Class.extend = function (/*Object*/ props) /*-> Class*/ {
 
 	NewClass.prototype = proto;
 
-	//inherit parent's statics
+	// inherit parent's statics
+	// 继承父类的静态方法
 	for (var i in this) {
+		// 判断this中是否有名称为i的属性，并且i不等于'prototype'
 		if (this.hasOwnProperty(i) && i !== 'prototype') {
 			NewClass[i] = this[i];
 		}
 	}
 
 	// mix static properties into the class
+	// 混合静态属性添加到类
 	if (props.statics) {
 		L.Util.extend(NewClass, props.statics);
 		delete props.statics;
 	}
 
 	// mix includes into the prototype
+	// 组合包括到原型
 	if (props.includes) {
 		L.Util.extend.apply(null, [proto].concat(props.includes));
 		delete props.includes;
 	}
 
 	// merge options
+	// 合并选项
 	if (props.options && proto.options) {
 		props.options = L.Util.extend({}, proto.options, props.options);
 	}
